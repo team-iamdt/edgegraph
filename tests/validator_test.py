e4ad4f99cee1832ae0c53aa6f42353f07cb2555b
Haze Lee <hazelee@realignist.me>
2022-08-13 23:25:21 +0900

    test(validator): add test for missing property

--- tests/validator_test.py
+++ tests/validator_test.py
@@ -221,3 +221,67 @@ async def test_validator_with_invalid_edgemodels(edgedb_dsn):
 
     await validator.aclose()
     assert len(e.value.errors) == 2
+
+
+@pytest.mark.asyncio
+async def test_validator_with_missed_properties(edgedb_dsn):
+    class UserModel(EdgeModel):
+        id: uuid.UUID = Field(default=uuid.uuid1())
+        updated_at: pendulum.DateTime = Field(default=pendulum.now())
+        created_at: pendulum.DateTime = Field(None)
+        deleted: bool = Field(default=False)
+
+        email: str
+        password: str
+        name: str = Field(min_length=3)
+
+        class Config:
+            module: str = "default"
+            name: str = "User"
+
+    class MemoModel(EdgeModel):
+        id: uuid.UUID = Field(default=uuid.uuid1())
+        updated_at: pendulum.DateTime = Field(default=pendulum.now())
+        deleted_at: t.Optional[pendulum.DateTime] = Field(default=None)
+
+        title: str
+        content: str
+        tags: t.List[str] = Field(default=[])
+
+        created_by: UserModel
+
+        class Config:
+            module: str = "default"
+            name: str = "Memo"
+
+    class CommentModel(EdgeModel):
+        id: uuid.UUID = Field(default=uuid.uuid1())
+        updated_at: pendulum.DateTime = Field(default=pendulum.now())
+        created_at: pendulum.DateTime = Field(None)
+        deleted_at: t.Optional[pendulum.DateTime] = Field(default=None)
+        deleted: bool = Field(default=False)
+
+        created_by: UserModel
+        memo: MemoModel
+        content: str
+
+        class Config:
+            module: str = "default"
+            name: str = "Comment"
+
+    validator = SchemaValidator(
+        edgedb_dsn,
+        models={UserModel, MemoModel, CommentModel},
+        tls_security="insecure",
+        fail_fast=False,
+        check_validation_rules=True,
+    )
+
+    with pytest.raises(ValidationError) as e:
+        await validator.validate()
+
+    await validator.aclose()
+
+    # Outline Error, 3 Properties Not Found in Memo.
+    # Outline Error, 1 Property Not Found in User.
+    assert len(e.value.errors) == 6
