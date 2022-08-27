29fa015c858d1e577454e8e380dc501385fe38b1
Haze Lee <hazelee@realignist.me>
2022-08-27 11:46:21 +0900

    feat(syntax): add base expression, query, shape

--- edgegraph/syntax/base.py
+++ edgegraph/syntax/base.py
@@ -1,5 +1,5 @@
 import typing as t
-from abc import abstractmethod
+from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 
 from edgegraph.types import QueryResult
@@ -8,7 +8,8 @@ from edgegraph.types import QueryResult
 # I used type ignore caused by https://github.com/python/mypy/pull/13398
 # still not released in library.
 @dataclass(frozen=True, init=False)  # type: ignore
-class Syntax:
+class BaseSyntax(metaclass=ABCMeta):
+    syntax_type: t.ClassVar[str]
     context: t.ClassVar[str]
     variables: t.Dict[str, t.Any]
 
